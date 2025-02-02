504bd5b0a965e815a8356f2523bc112d8b80123a
kofrah <kofrah20@gmail.com>
2025-02-02 20:56:45 +0900

    検索条件が初期の場合はリセットしない

--- src/app/hooks/useResetSearch.ts
+++ src/app/hooks/useResetSearch.ts
@@ -1,12 +1,20 @@
 import { useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export function useResetSearch() {
   const [isClicked, setIsClicked] = useState(false);
   const { replace } = useRouter();
   const pathname = usePathname();
 
+  const currentParams = useSearchParams();
+
   const handleReset = () => {
+    // pageが1かつqueryがない場合はなにもしない
+    if (currentParams.get("page") === "1" && !currentParams.get("query")) {
+      console.log("Already reset");
+      return;
+    }
+
     if (isClicked) {
       console.log("Already clicked");
       return;
@@ -15,13 +23,14 @@ export function useResetSearch() {
     setIsClicked(true);
 
     try {
+      console.log("Reset search excute");
       const params = new URLSearchParams();
       params.set("page", "1");
       params.delete("query");
       replace(`${pathname}?${params.toString()}`);
 
       // クリック防止を一定時間後に解除
-      setTimeout(() => setIsClicked(false), 10000);
+      setTimeout(() => setIsClicked(false), 1000);
     } catch (error) {
       console.error("Navigation failed", error);
       setIsClicked(false);
