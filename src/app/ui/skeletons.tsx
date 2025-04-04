5a1a683052ff94b4f21a7d0d0da32186266eae98
kofrah <kofrah20@gmail.com>
2025-04-04 23:06:56 +0900

    お気に入りページ:スケルトン微調整

--- src/app/ui/skeletons.tsx
+++ src/app/ui/skeletons.tsx
@@ -15,27 +15,40 @@ const shimmerInline = `
 
 export function TableSkeleton() {
   return (
-    <div className="px-4 pt-28 md:pt-16">
+    <div className="w-full px-4 pt-28 md:pt-16">
       <div className="text-center px-2 mb:px-4">
         <span
           className={`inline-block h-6 w-56 rounded bg-gray-200 dark:bg-gray-700 ${shimmerInline}`}
         />
       </div>
+
       <div className="grid grid-cols-2 md:grid-cols-4 gap-3 gap-y-6 pt-4">
         {Array.from({ length: 12 }).map((_, i) => (
           <div
             key={i}
-            className="p-2 rounded-md bg-white dark:bg-gray-800 shadow-sm"
+            className="
+              p-1 rounded-sm bg-white dark:bg-gray-800 shadow-sm
+              flex flex-col"
           >
-            <div
-              className={`aspect-[3/4] bg-gray-200 dark:bg-gray-700 rounded mb-2 ${shimmer}`}
-            />
-            <div
-              className={`h-4 bg-gray-200 dark:bg-gray-700 rounded w-2/3 mb-1 ${shimmer}`}
-            />
-            <div
-              className={`h-4 bg-gray-200 dark:bg-gray-700 rounded w-1/3 ${shimmer}`}
-            />
+            <div className="flex justify-center items-center border-black">
+              <div
+                className={`w-full aspect-[3/4] bg-gray-200 dark:bg-gray-700 rounded ${shimmer}`}
+              />
+            </div>
+
+            <div className="text-black dark:text-white mt-1 flex justify-between items-center">
+              <div className="flex flex-col gap-1 w-full">
+                <div
+                  className={`h-3 md:h-4 bg-gray-200 dark:bg-gray-700 rounded w-2/3 ${shimmer}`}
+                />
+                <div
+                  className={`h-4 bg-gray-200 dark:bg-gray-700 rounded w-1/3 ${shimmer}`}
+                />
+              </div>
+              <div
+                className={`h-5 w-5 bg-gray-200 dark:bg-gray-700 rounded-full ml-2 ${shimmer}`}
+              />
+            </div>
           </div>
         ))}
       </div>
